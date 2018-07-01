ebe1b60a54027961e391f3d030ce701fe9a12fca
Michael Gardner <mikejr83@gmail.com>
2018-07-01 14:49:46 -0400

    Modifying the packets structure to reflect new knowledge

--- src/models/color-packet.ts
+++ src/models/color-packet.ts
@@ -1,26 +1,41 @@
 import { Packet } from "./packet";
 
 export class ColorPacket extends Packet {
-  constructor(private key: number, private channel: number, private color: number) {
+  constructor(
+    private key: number,
+    private colorChannelId: number,
+    private toColor?: number,
+    private fromColor?: number,
+    private animationTime?: number,
+    private animationTimeMultiplier?: number,
+    private animationStepWidth?: number,
+    private animationSleep?: number) {
     super();
   }
 
   public buildPacketBytes(): number[] {
+    const toColor = this.toColor || 0x00;
+    const fromColor = this.fromColor || toColor;
+    const animationTime = this.animationTime || 0xFF;
+    const animationTimeMultiplier = this.animationTimeMultiplier || 0x00;
+    const animationStepWidth = this.animationStepWidth || 0x00;
+    const animationSleep = this.animationSleep || 0x00;
+    
     return [
       0x00,
       0x28,
       0x00,
-      this.channel,
+      this.colorChannelId,
       0x01,
       this.key,
       0x02, /* 0x00 = off, 0x01 = stop animation, 0x02 = start animation */
-      this.color, /* fade too */
+      toColor, /* fade too */
       0x00,
-      0xFF, /* animation speed; 0x00 = slowest, 0xFF = fastest */
-      0x00,
-      0x00,
-      0x00,
-      this.color, /* fade from */
+      animationTime, /* animation speed; 0x00 = slowest, 0xFF = fastest */
+      animationTimeMultiplier,
+      animationStepWidth,
+      animationSleep,
+      fromColor, /* fade from */
       0x00,
       0x00,
       0x00,
