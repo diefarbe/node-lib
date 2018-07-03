b9e941e557c02ab9f2516744bff941346f79ff79
chris13524 <chris@hichris.com>
2018-07-02 21:05:15 -0400

    Added code to test flash a specific key certain colors (to figure out mappings). Added sparkle animation script.

--- POC/ts/index.ts
+++ POC/ts/index.ts
@@ -1,4 +1,5 @@
 import {Keyboard, Keys} from "../../src";
+import {Key} from "../../src/models/key";
 
 // Ok. Let's create a keyboard
 // This will be done with the default values for USB vendor, product, and interface
@@ -33,3 +34,51 @@ keyboard.apply();
  * Clean up!
  */
 keyboard.close();
+
+/*
+This function is useful for figuring out key mappings. While running, the key should flash red, green, blue, blank, repeat.
+
+testKey(new Key(5, 0, 1, 2));
+ */
+function testKey(key: Key) {
+  while (true) {
+    keyboard.setRgbHex(key, 0xFF0000);
+    keyboard.apply();
+    sleep(300);
+    keyboard.setRgbHex(key, 0x00FF00);
+    keyboard.apply();
+    sleep(300);
+    keyboard.setRgbHex(key, 0x0000FF);
+    keyboard.apply();
+    sleep(300);
+    keyboard.setRgbHex(key, 0x000000);
+    keyboard.apply();
+    sleep(1000);
+  }
+}
+
+function sparkle() {
+  let keys = Object.keys(Keys);
+  while (true) {
+    let chosenKey = keys[Math.floor(Math.random() * keys.length)];
+    let key = Keys[chosenKey];
+    let color = Math.floor(Math.random() * 3);
+    if (color == 0) {
+      keyboard.setRgbHex(key, 0xFF0000);
+    } else if (color == 1) {
+      keyboard.setRgbHex(key, 0x00FF00);
+    } else if (color == 2) {
+      keyboard.setRgbHex(key, 0x0000FF);
+    } else {
+      throw new Error("should never happen")
+    }
+    keyboard.apply();
+  }
+}
+
+function sleep(sleepDuration: number) {
+  let now = new Date().getTime();
+  while (new Date().getTime() < now + sleepDuration) {
+    /* do nothing */
+  }
+}
