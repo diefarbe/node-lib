a526b53b6378302cf9ac2bf0c00f3c4cecef095c
Michael Gardner <mikejr83@gmail.com>
2018-07-03 22:08:01 -0400

    Adding en-GB map
    This keymap needs work. It is preliminary.
    
    The key names in the hash have been changed to
    description.replace(/\W/g, "") (only 0-9a-zA-Z_).

--- src/internal/data/index.ts
+++ src/internal/data/index.ts
@@ -1,13 +1,22 @@
 import { IKeyModel, KeyModel } from "../models/key-model";
 
-import enData from "./en-us.json";
+import enUsData from "./en-us.json";
+import enGbData from "./en-us.json";
 
 export interface IKeyMapCulture {
   [keyName: string]: KeyModel;
 }
 
-export const enKeyMapData: IKeyMapCulture = {};
+export const enUsKeyMapData: IKeyMapCulture = makeCulture(enUsData);
+export const enGbKeyMapData: IKeyMapCulture = makeCulture(enGbData);
 
-for (const keymapping of (enData as IKeyModel[])) {
-  enKeyMapData[keymapping.shortName] = new KeyModel(keymapping);
+function makeCulture(models: IKeyModel[]): IKeyMapCulture {
+  const data: IKeyMapCulture = {};
+
+  for (const keymapping of models) {
+    const keyName = keymapping.description.replace(/\W/g, "");
+    data[keyName] = new KeyModel(keymapping);
+  }
+
+  return data;
 }
--- src/key-info.ts
+++ src/key-info.ts
@@ -1,9 +1,14 @@
-import { enKeyMapData, IKeyMapCulture } from "./internal/data";
+import { 
+    enGbKeyMapData,
+    enUsKeyMapData, 
+    IKeyMapCulture 
+} from "./internal/data";
 import { KeyModel } from "./internal/models";
 
 /**
  * Maps a common name of a key to a key object.
  */
 export const KeyInfo: { [culture: string]: IKeyMapCulture } = {
-    "en-US": enKeyMapData
+    "en-GB": enGbKeyMapData,
+    "en-US": enUsKeyMapData
 };
