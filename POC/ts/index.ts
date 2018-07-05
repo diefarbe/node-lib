8bcf2317648816a8d0facecd297efa523ae3173e
Michael Gardner <mikejr83@gmail.com>
2018-07-04 22:24:12 -0400

    Working on queue

--- POC/ts/index.ts
+++ POC/ts/index.ts
@@ -1,35 +1,49 @@
 import { Keyboard, KeyInfo, KeyState } from "../../src";
 import { KeyModel } from "../../src/internal/models";
 
-// Ok. Let's create a keyboard
-// This will be done with the default values for USB vendor, product, and interface
-const keyboard = new Keyboard();
+async function keyboardPoC() {
+  // Ok. Let's create a keyboard
+  // This will be done with the default values for USB vendor, product, and interface
+  const keyboard = new Keyboard();
 
-/*
- * Now, this actually finds the keyboard. Internally the keyboard object will track
- * the device, but if you really want access to the low-level HID device this will 
- * return it.
- * 
- * NOTE: THE FIND METHOD CAN TAKE THE VENDOR AND PRODUCT IDS ALONG WITH A HOST OF USB
- * INFO TO MAKE THE CONNECTION. THE DEFAULT CALL USES THE INFO NECESSARY TO CONNECT
- * TO THE Q5.
- */
-const hidDevice = keyboard.find();
+  /*
+   * Now, this actually finds the keyboard. Internally the keyboard object will track
+   * the device, but if you really want access to the low-level HID device this will 
+   * return it.
+   * 
+   * NOTE: THE FIND METHOD CAN TAKE THE VENDOR AND PRODUCT IDS ALONG WITH A HOST OF USB
+   * INFO TO MAKE THE CONNECTION. THE DEFAULT CALL USES THE INFO NECESSARY TO CONNECT
+   * TO THE Q5.
+   */
+  const hidDevice = keyboard.find();
 
-/*
- * Now that we found our keyboard let's go and initialize it...
- * What initialization means, I'm not sure yet, but we send some stuff...
- */
-keyboard.initialize();
+  /*
+   * Now that we found our keyboard let's go and initialize it...
+   * What initialization means, I'm not sure yet, but we send some stuff...
+   */
+  keyboard.initialize();
 
-allColor("#FF0000");
-allColor("#00FF00");
-allColor("#0000FF");
+  await allColor(keyboard, "#4F00F0");
+
+  //await sparkleBatch(keyboard);
+
+  const data = keyboard.getKeyboardData();
+  console.log("Firmware Version:" + data.firmware);
+
+  /*
+   * Clean up!
+   */
+  keyboard.close();
+}
+
+keyboardPoC();
+
+// allColor("#00FF00");
+// allColor("#0000FF");
 
 // sparkle();
-const data = keyboard.getKeyboardData();
-console.log("Firmware Version:" + data.firmware);
-keyboard.setBrightness(63);
+
+// keyboard.setBrightness(63);
 
 // sparkleBatch();
 
@@ -51,19 +65,14 @@ keyboard.setBrightness(63);
 /*
  * Great! Apply our changes!
  */
-keyboard.apply();
-
-/*
- * Clean up!
- */
-keyboard.close();
+// keyboard.apply();
 
 /*
 This function is useful for figuring out key mappings. While running, the key should flash red, green, blue, blank, repeat.
 
 testKey(new Key(5, 0, 1, 2));
  */
-function testKey(key: KeyModel) {
+function testKey(keyboard: Keyboard, key: KeyModel) {
   while (true) {
     keyboard.setKeyState(new KeyState(key).setFromColorHex("FF0000").setToColorHex("FF0000"));
     keyboard.apply();
@@ -80,7 +89,7 @@ function testKey(key: KeyModel) {
   }
 }
 
-function sparkle() {
+function sparkle(keyboard: Keyboard) {
   const keys = Object.keys(KeyInfo["en-US"]);
   while (true) {
     const chosenKey = keys[Math.floor(Math.random() * keys.length)];
@@ -100,7 +109,7 @@ function sparkle() {
   }
 }
 
-function sparkleBatch() {
+async function sparkleBatch(keyboard: Keyboard) {
   const keys = Object.keys(KeyInfo["en-US"]);
   const currentKeys: KeyModel[] = [];
   while (true) {
@@ -108,11 +117,11 @@ function sparkleBatch() {
     const key = KeyInfo["en-US"][chosenKey];
     const color = Math.floor(Math.random() * 3);
     if (color === 0) {
-      keyboard.setKeyState(new KeyState(key).setToColorHex("#FF0000"));
+      await keyboard.setKeyState(new KeyState(key).setToColorHex("#FF0000"));
     } else if (color === 1) {
-      keyboard.setKeyState(new KeyState(key).setToColorHex("#00FF00"));
+      await keyboard.setKeyState(new KeyState(key).setToColorHex("#00FF00"));
     } else if (color === 2) {
-      keyboard.setKeyState(new KeyState(key).setToColorHex("#0000FF"));
+      await keyboard.setKeyState(new KeyState(key).setToColorHex("#0000FF"));
     } else {
       throw new Error("should never happen");
     }
@@ -120,18 +129,21 @@ function sparkleBatch() {
     currentKeys.push(key);
 
     if (currentKeys.length > 10) {
-      keyboard.setKeyState(new KeyState(currentKeys.splice(0, 1)[0]).setToColorHex("#000000"));
+      await keyboard.setKeyState(new KeyState(currentKeys.splice(0, 1)[0]).setToColorHex("#000000"));
       keyboard.apply();
     }
   }
 }
 
-function allColor(hexColor: string) {
+async function allColor(keyboard: Keyboard, hexColor: string): Promise<void> {
+  const promises: Array<Promise<void>> = [];
   const keys = Object.keys(KeyInfo["en-US"]);
   for (const keyName of keys) {
     const key = KeyInfo["en-US"][keyName];
-    keyboard.setKeyState(new KeyState(key).setToColorHex(hexColor));
+    promises.push(keyboard.setKeyState(new KeyState(key).setToColorHex(hexColor)));
   }
+  await Promise.all(promises);
+
   keyboard.apply();
 }
 
--- src/keyboard.ts
+++ src/keyboard.ts
@@ -1,3 +1,5 @@
+import PQueue from "p-queue";
+
 import { BrightnessPacket } from "./internal/models/packets/brightness-packet";
 import { FirmwarePacket } from "./internal/models/packets/firmware-packet";
 import { FreezePacket } from "./internal/models/packets/freeze-packet";
@@ -7,10 +9,28 @@ import { KeyState } from "./key-state";
 
 import { findUsbDevice, Usb } from "./usb";
 
-export class Keyboard {
-  private usbDevice: Usb | undefined;
-  private sequence: number = 0;
+type Action = () => void;
 
+export class Keyboard {
+  private isInitialized: boolean = false;
+  private sequence: number = 0;
+  private usbDevice: Usb | undefined;
+
+  private intervalRef: NodeJS.Timer | undefined;
+
+  private queue = new PQueue({
+    autoStart: false,
+    concurrency: 1,
+  });
+  /**
+   * Find a keyboard.
+   *
+   * @param {number} [vendorId] For DAS the id is 0x24F0
+   * @param {number} [productId] Ex: For the Q5 the id is 0x2020
+   * @param {number} [deviceInterface]
+   * @param {number} [usage]
+   * @memberof Keyboard
+   */
   public find(vendorId?: number, productId?: number, deviceInterface?: number, usage?: number): Usb {
     this.usbDevice = findUsbDevice(vendorId || 0x24f0,
       productId || 0x2020,
@@ -18,7 +38,6 @@ export class Keyboard {
       usage || 165);
 
     this.usbDevice.connect();
-
     return this.usbDevice;
   }
 
@@ -31,19 +50,46 @@ export class Keyboard {
     }
 
     this.featureReports(new InitializePacket().buildPacketBytes());
+
+    this.isInitialized = true;
+    this.queue.start();
   }
 
   /**
    * Sets an array of KeyStates to the keyboard
    */
-  public setKeyState(states: KeyState[] | KeyState) {
+  public setKeyState(states: KeyState[] | KeyState): Promise<void> {
+    let promise: Promise<void>;
+
     if (Array.isArray(states)) {
+      const promises: Array<Promise<void>> = [];
+
       for (const state of states) {
-        this.executePackets(state);
+        const p = this.queue.add(() => {
+          return new Promise<void>((resolve, reject) => {
+            setTimeout(() => {
+              this.executePackets(state);
+              resolve();
+            }, 100);
+          });
+        }) as Promise<void>;
+
+        promises.push(p);
       }
+
+      promise = Promise.all(promises).then(() => { return; });
     } else {
-      this.executePackets(states);
+      promise = this.queue.add(() => {
+        return new Promise<void>((resolve, reject) => {
+          setTimeout(() => {
+            this.executePackets(states);
+            resolve();
+          }, 100);
+        });
+      }) as Promise<void>;
     }
+
+    return promise;
   }
 
   /**
@@ -83,31 +129,51 @@ export class Keyboard {
    */
   public close() {
     if (typeof this.usbDevice !== "undefined") {
+      this.isInitialized = false;
+      if (this.intervalRef !== undefined) {
+        clearInterval(this.intervalRef);
+      }
       this.usbDevice.disconnect();
       this.usbDevice = undefined;
     }
   }
 
-  private executePackets(state: KeyState) {
-    for (const aPacket of state.build()) {
+  private executePackets(keyState: KeyState) {
+    for (const aPacket of keyState.build()) {
       this.featureReports(aPacket);
     }
   }
 
   private featureReports(report: number[]) {
+    // Let's create the buffer
     const buff = [0 /* report id */, ...report];
+    // Pad out to 64 bytes
     while (buff.length < 65) {
       buff.push(0);
     }
+    // Place our sequence number into the packet
     buff[3] = this.sequence;
-
+    // Send the packet to the device;
     this.writeDataToDevice(buff);
+    /*
+      Next, we're going to read from the device. This is done so that we can make sure
+      the device accepted our write.
+     */
     const res = this.readDataFromDevice();
+    /*
+      The idea here is that the device should acknowledge our command.
+
+      NEED TO EXPLAIN WHY WE'RE LOOKING AT POSITION 2 AND WHY IT SHOULDN'T BE 0x14.
+
+      When looking at the read we shouldn't see our sequence in position 3.
+     */
     if (res[2] !== 0x14 || res[3] !== this.sequence) {
       throw new Error("no ack " + this.sequence);
     }
 
+    // Increement the sequenece
     this.sequence++;
+    // Make sure we don't overflow the sequence value!
     if (this.sequence > 0xFF) { this.sequence = 0x00; }
   }
 
@@ -115,7 +181,12 @@ export class Keyboard {
     if (typeof this.usbDevice === "undefined") {
       throw new Error("Not connected to device");
     }
-    return this.usbDevice.read();
+    try {
+      return this.usbDevice.read();
+    } catch(e) {
+      console.warn(e);
+      return [];
+    }
   }
 
   private writeDataToDevice(data: number[]) {
