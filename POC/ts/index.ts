184858f4ee5d7d4089c1da56d094be920b6827ec
Rzah <rob@solve-id.net>
2018-07-11 18:10:18 +0100

    galaxy and shuffle

--- POC/ts/index.ts
+++ POC/ts/index.ts
@@ -33,6 +33,10 @@ keyboard.setBrightness(63);
 
 // sparkleBatch();
 
+// cosmos themed profile
+// galaxy('uk');
+// galaxy('us');
+
 /*
  * Let's set some color!
  */
@@ -123,6 +127,78 @@ function sparkleBatch() {
       keyboard.setKeyState(new KeyState(currentKeys.splice(0, 1)[0]).setToColorHex("#000000"));
       keyboard.apply();
     }
+  }
+}
+
+function shuffle(array: any): any {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function galaxy(region: string) {
+  console.log('running galaxy');
+  allColor("#000000");
+  var mySelector = [];
+  for (var i = 0; i < 216; i++) {
+    mySelector.push(i);
+  }
+
+  if (region == 'uk') {
+    var ki = Object.keys(KeyInfo["en-GB"]);
+  } else {
+    var ki = Object.keys(KeyInfo["en-US"]);  
+  }
+  
+  ki = shuffle(ki);
+  var ffs = ki.length;
+  var color_array = [];
+  var target_array = [];
+  var delay_array = [];
+  let randomKey: any = 0;
+  const c = ['7D008E', '1A0478', '6B5D9E', '9398D7', '3F70B4'];
+  const d = ['140751', '160124', '140034', '07001D', '30053F'];
+  for (var i = 0; i < ffs; i++) {
+    target_array.push([ki[i]]);
+    delay_array.push(i*20);
+    color_array.push(c[Math.floor(Math.random() * c.length)]);
+  }
+
+  for (var index = 0; index < color_array.length; index++) {
+    var color_arg = color_array[index];
+    var key_array = target_array[index];
+    var startDelay = delay_array[index];
+    var back_color = (d[Math.floor(Math.random() * d.length)]);
+    
+    for (var i = 0; i < key_array.length; i++) {
+      if (region == 'uk') {
+        var key = KeyInfo["en-GB"][key_array[i]];
+      } else {
+        var key = KeyInfo["en-US"][key_array[i]]; 
+      }
+
+      keyboard.setKeyState(new KeyState(key)
+        .setToColorHex("#"+color_arg)
+        .setFromColorHex("#"+back_color)
+        .setUpMaximum("#"+back_color)
+        .setUpIncrement(50)
+        .setUpIncrementDelay(10)
+        .setDownDecrement(50)
+        .setDownDecrementDelay(5)
+        .setUpHoldDelay(5)
+        .setDownHoldDelay(900)
+        .setStartDelay(startDelay)
+        .setTransitionReverse()
+        .setApplyDelayed()
+        );
+    }
+    
   }
 }
 
